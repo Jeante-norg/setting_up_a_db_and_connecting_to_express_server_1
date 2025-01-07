82f3655eadb3f48bbf155be9f4f42a9f5adea718
Tej <jupitersorbeet@gmail.com>
2025-01-07 06:44:37 +0530

    set up a database and connected to express server

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-const express = require('express');
-const { resolve } = require('path');
+const express = require("express");
+const { resolve } = require("path");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+const mongoose = require("mongoose");
 
 const app = express();
-const port = 3010;
+const port = process.env.PORT || 3010;
 
-app.use(express.static('static'));
+app.use(express.static("static"));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+mongoose
+  .connect(process.env.CONN_STR, {
+    useNewUrlParser: true,
+  })
+  .then((conn) => {
+    console.log("Connected to Database");
+  })
+  .catch((err) => {
+    console.log("Error connecting to database:", err.message);
+  });
+
+app.get("/", (req, res) => {
+  res.sendFile(resolve(__dirname, "pages/index.html"));
 });
 
 app.listen(port, () => {
